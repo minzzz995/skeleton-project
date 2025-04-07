b47bb4d68d8ff08072e9089666d95c5f6bfe6c48
Seonghoon Jeong <hooni09061201@gmail.com>
2025-04-07 13:48:59 +0900

    remove_test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,6 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h2> hi s.h.</h2>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
