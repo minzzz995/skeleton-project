154e1d9e7663baf055eb4cab531e2ce761982361
snowman <rhun00moon@gmail.com>
2025-04-09 14:15:13 +0900

    transactions.vue 수정
    transactions.vue 수정 및 transactionsStore.js 수정

--- src/pages/Transactions.vue
+++ src/pages/Transactions.vue
@@ -1,12 +1,10 @@
 <script setup>
-
 import { useTransactionStore } from "@/store/transactionStore";
 import { useCategoryStore } from "@/store/categoryStore";
 import TransactionList from "@/components/Transaction/TransactionList.vue";
 import TransactionModal from "@/components/Transaction/TransactionModal.vue";
 import { ref, computed, onMounted } from "vue";
 import dayjs from "dayjs";
-
 
 // Store 연결
 const transactionStore = useTransactionStore();
@@ -15,29 +13,85 @@ const categoryStore = useCategoryStore();
 // 날짜 초기값 설정
 const now = dayjs();
 const dateRange = ref({
-  start: now.startOf('month').format('YYYY-MM-DD'),
-  end: now.format('YYYY-MM-DD'),
+  start: now.startOf("month").format("YYYY-MM-DD"),
+  end: now.format("YYYY-MM-DD"),
 });
 
 // 상태 변수
 const modalVisible = ref(false);
-const selectedTransaction = ref(null);
-const selectedCategory = ref('');
+const selectedBudget = ref(null);
+const selectedCategory = ref("");
 const showStartPicker = ref(false);
 const showEndPicker = ref(false);
 
 // 계산된 값
+const groupedBudgets = computed(() => transactionStore.groupByDate);
+const summary = computed(() => transactionStore.summary);
 const incomeCategories = computed(() => categoryStore.incomeCategories);
 const expenseCategories = computed(() => categoryStore.expenseCategories);
+const formattedStart = computed(() =>
+  dayjs(dateRange.value.start).format("M월 D일")
+);
+const formattedEnd = computed(() =>
+  dayjs(dateRange.value.end).format("M월 D일")
+);
 
 // 초기 데이터 로딩
 onMounted(async () => {
-  //await transactionStore.fetchTransactions()
+  await transactionStore.fetchBudgets();
   await categoryStore.fetchCategories();
-  //applyFilters();
+  applyFilters();
 });
 
 // 메서드 정의
+function openAddModal() {
+  selectedBudget.value = null;
+  modalVisible.value = true;
+}
+
+function openEditModal(budget) {
+  selectedBudget.value = budget;
+  modalVisible.value = true;
+}
+
+async function deleteBudget(id) {
+  await transactionStore.deleteBudget(id);
+}
+
+function moveMonth(offset) {
+  const newStart = dayjs(dateRange.value.start)
+    .add(offset, "month")
+    .startOf("month");
+  const newEnd = newStart.endOf("month");
+  dateRange.value.start = newStart.format("YYYY-MM-DD");
+  dateRange.value.end = newEnd.format("YYYY-MM-DD");
+  applyFilters();
+}
+
+function handleStartDateSelect(date) {
+  dateRange.value.start = date;
+  showStartPicker.value = false;
+  applyFilters();
+}
+
+function handleEndDateSelect(date) {
+  dateRange.value.end = date;
+  showEndPicker.value = false;
+  applyFilters();
+}
+
+function updateCategory() {
+  transactionStore.setCategoryFilter(selectedCategory.value);
+}
+
+function applyFilters() {
+  transactionStore.setDateRange(dateRange.value.start, dateRange.value.end);
+  transactionStore.setCategoryFilter(selectedCategory.value);
+}
+
+function format(value) {
+  return parseInt(value).toLocaleString() + "원";
+}
 </script>
 
 <template>
@@ -47,11 +101,9 @@ onMounted(async () => {
         <button @click="moveMonth(-1)">
           <i class="fa fa-chevron-left" aria-hidden="true"></i>
         </button>
-        <span>2025.02.01</span
-        ><!-- <span>{{ formattedStart }}</span> -->
+        <span>{{ formattedStart }}</span>
         ~
-        <span>2025.02.10</span
-        ><!-- <span>{{ formattedEnd }}</span> -->
+        <span>{{ formattedEnd }}</span>
         <button @click="moveMonth(1)">
           <i class="fa fa-chevron-right" aria-hidden="true"></i>
         </button>
@@ -83,26 +135,26 @@ onMounted(async () => {
     <div class="summary">
       <div class="summary-box">
         <p>수입</p>
-        200<!-- {{ format(summary.income) }} -->
+        {{ format(summary.income) }}
       </div>
       <div class="summary-box">
         <p>지출</p>
-        200<!-- {{ format(summary.income) }} -->
+        {{ format(summary.expense) }}
       </div>
       <div class="summary-box">
         <p>순이익</p>
-        200<!-- {{ format(summary.income) }} -->
+        {{ format(summary.net) }}
       </div>
     </div>
     <TransactionList
-      :transactions="groupTransections"
+      :transactions="groupedBudgets"
       @edit="openEditModal"
-      @delete="deleteTransaction"
+      @delete="deleteBudget"
     />
     <button class="add-btn" @click="openAddModal">거래 추가</button>
     <TransactionModal
       v-if="modalVisible"
-      :editTarget="selectedTransaction"
+      :editTarget="selectedBudget"
       @close="modalVisible = false"
     />
   </div>
