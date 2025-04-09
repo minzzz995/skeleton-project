723e7ff004477b580f4fb259b2f486c770a8af4d
seul <angel3030303@gmail.com>
2025-04-09 15:43:25 +0900

    모바일 크기에서 스크롤바 두개 생기는 이슈

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -10,7 +10,7 @@
     <SideBar class="d-none d-md-block" />
 
     <!-- 메인 콘텐츠 -->
-    <main class="flex-fill p-3">
+    <main class="flex-fill" style="overflow: hidden">
       <slot />
     </main>
   </div>
