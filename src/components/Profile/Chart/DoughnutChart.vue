18f10f24889b6dfc7ec4f7e0a0e47c88aaa1ece9
Todom2 <schoolpang@naver.com>
2025-04-09 13:12:02 +0900

    docs: file structure changed

---
+++ src/components/Profile/Chart/DoughnutChart.vue
@@ -0,0 +1,23 @@
+<!-- src/components/common/charts/DoughnutChart.vue -->
+<template>
+  <Chart type="doughnut" :data="chartData" :options="options" />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { Chart } from 'vue-chartjs';
+import {
+  DoughnutController,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Chart as ChartJS,
+} from 'chart.js';
+
+ChartJS.register(DoughnutController, ArcElement, Tooltip, Legend);
+
+defineProps({
+  chartData: Object,
+  options: Object,
+});
+</script>
