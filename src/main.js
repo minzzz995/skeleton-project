a714eb0a4814925f9c5d759334db711a085082a4
snowman <rhun00moon@gmail.com>
2025-04-10 09:24:18 +0900

    src/main.js 수정
    src/main.js 수정

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-import './assets/main.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import VueApexCharts from "vue3-apexcharts";
+
+import "bootstrap/dist/css/bootstrap.css";
+import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@/assets/styles/fonts.css";
+import "./assets/main.css";
 
 window.bootstrap = bootstrap;
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
-import App from './App.vue';
-import router from './router';
-import VueApexCharts from 'vue3-apexcharts';
-import '@/assets/styles/fonts.css';  
-import './assets/main.css';       
 
 const app = createApp(App);
 
@@ -19,4 +18,4 @@ app.use(createPinia());
 app.use(router);
 app.use(VueApexCharts);
 
-app.mount('#app');
+app.mount("#app");
